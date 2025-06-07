6f6844bb5a6666650eb355aa14e1b48746378b19
Beneguen75 <115241272+Beneguen75@users.noreply.github.com>
2025-06-07 19:44:15 -0300

    Create migration.sql

---
+++ backend/prisma/migrations/20250606015850_init_movie_model/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Movie" (
+    "id" TEXT NOT NULL,
+    "titulo" TEXT NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "genero" TEXT NOT NULL,
+    "classificacao" TEXT NOT NULL,
+    "duracao" INTEGER NOT NULL,
+    "dataEstreia" TIMESTAMP(3) NOT NULL,
+    "posterUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Movie_pkey" PRIMARY KEY ("id")
+);
