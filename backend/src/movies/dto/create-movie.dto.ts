35692f2f1fca7338a00135b815a3bc06a7ca5ac5
Beneguen75 <115241272+Beneguen75@users.noreply.github.com>
2025-06-07 19:44:23 -0300

    Create create-movie.dto.ts

---
+++ backend/src/movies/dto/create-movie.dto.ts
@@ -0,0 +1,31 @@
+import { IsString, IsNotEmpty, IsInt, IsDateString, IsOptional, IsUrl } from 'class-validator';
+
+export class CreateMovieDto {
+  @IsString()
+  @IsNotEmpty()
+  titulo: string;
+
+  @IsString()
+  @IsNotEmpty()
+  descricao: string;
+
+  @IsString()
+  @IsNotEmpty()
+  genero: string;
+
+  @IsString()
+  @IsNotEmpty()
+  classificacao: string;
+
+  @IsInt()
+  @IsNotEmpty()
+  duracao: number;
+
+  @IsDateString()
+  @IsNotEmpty()
+  dataEstreia: string;
+
+  @IsOptional()
+  @IsUrl()
+  posterUrl?: string;
+}
