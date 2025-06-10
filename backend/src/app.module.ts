7bcb0c1673d43fd87597ca2e185d5ed53d3285f2
Beneguen75 <115241272+Beneguen75@users.noreply.github.com>
2025-06-10 19:05:59 -0300

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { MoviesModule } from './movies/movies.module';
+import { RoomsModule } from './rooms/rooms.module';
 
 @Module({
-  imports: [PrismaModule, MoviesModule],
+  imports: [PrismaModule, MoviesModule, RoomsModule],
   controllers: [AppController],
   providers: [AppService],
 })
