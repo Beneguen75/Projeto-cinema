808404cd49ac0c4873149c50953291519092875c
Beneguen75 <115241272+Beneguen75@users.noreply.github.com>
2025-06-10 19:05:47 -0300

    Create rooms.service.ts

---
+++ backend/src/rooms/rooms.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateRoomDto } from './dto/create-room.dto';
+import { UpdateRoomDto } from './dto/update-room.dto';
+import { Room } from '././entities/room.entity';
+
+@Injectable()
+export class RoomsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createRoomDto: CreateRoomDto): Promise<Room> {
+    return this.prisma.room.create({ data: createRoomDto });
+  }
+
+  async findAll(): Promise<Room[]> {
+    return this.prisma.room.findMany();
+  }
+
+  async findOne(id: string): Promise<Room> {
+    const room = await this.prisma.room.findUnique({ where: { id } });
+    if (!room) {
+      throw new NotFoundException(`Sala com ID "${id}" não encontrada.`);
+    }
+    return room;
+  }
+
+  async update(id: string, updateRoomDto: UpdateRoomDto): Promise<Room> {
+    await this.findOne(id); // Garante que a sala existe antes de tentar atualizar
+    return this.prisma.room.update({
+      where: { id },
+      data: updateRoomDto,
+    });
+  }
+
+  async remove(id: string): Promise<Room> {
+    await this.findOne(id); // Garante que a sala existe antes de tentar remover
+    return this.prisma.room.delete({ where: { id } });
+  }
+}
