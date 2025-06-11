4feed310b59155da67b9244c70b9d95606253ce6
Beneguen75 <115241272+Beneguen75@users.noreply.github.com>
2025-06-11 01:32:13 -0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './router/AppRouter';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 // Importações para react-toastify
@@ -12,8 +11,6 @@ const App: React.FC = () => {
   return (
     <BrowserRouter>
       <AppRouter />
-      {/* Adicione o ToastContainer aqui, fora do Router mas dentro do BrowserRouter se quiser */}
-      {/* Ou pode ser no final do AppRouter, ou dentro de um MainLayout global */}
       <ToastContainer
         position="top-right" // Posição das notificações
         autoClose={3000} // Fecha automaticamente após 3 segundos
