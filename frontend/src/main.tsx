f38d7f14dc6c5851a96470dfe36cf6e0b0ef82f9
Beneguen75 <115241272+Beneguen75@users.noreply.github.com>
2025-06-11 01:32:34 -0300

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// Se você não importou o index.css no App.tsx, pode ser aqui.
-// Mas é comum manter as importações de CSS mais perto do App.tsx.
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
