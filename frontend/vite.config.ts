4177c0c777c2cc397184dc550a62f3c578f76654
Beneguen75 <115241272+Beneguen75@users.noreply.github.com>
2025-06-17 20:50:39 -0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public',
+  server: {
+    host: '0.0.0.0', 
+
+    allowedHosts: ['frontend'], 
+  },
 })
